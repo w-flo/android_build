e56608c725616050743325ddaffc6496aa67726e
Ricardo Salveti de Araujo <ricardo.salveti@canonical.com>
2013-01-03 15:09:21 -0200

    Enabling more hybris specific components
    Change-Id: I9a4d8f0b56750ec84119943dc6d98b49f301774b
    Signed-off-by: Ricardo Salveti de Araujo <ricardo.salveti@canonical.com>

--- target/product/core.mk
+++ target/product/core.mk
@@ -129,6 +129,9 @@ PRODUCT_PACKAGES += \
     zoneinfo.dat \
     zoneinfo.idx \
     zoneinfo.version \
+    ubuntuappmanager \
+    libinput \
+    libubuntu_application_api \
     libsf_compat_layer \
     ubuntu_chroot \
     uchroot
