5faffcd0599d0590da6cf672d2acb10acf5328ad
Dmitrijs Ledkovs <dmitrijs.ledkovs@canonical.com>
2013-10-01 14:15:12 +0100

    Include sdcard build for the emulator.
    Signed-off-by: Dmitrijs Ledkovs <dmitrijs.ledkovs@canonical.com>

--- tools/build-emulator-sdcard.sh
+++ tools/build-emulator-sdcard.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 set -e
+
+[ -n "$1" ] && cd "$1"
+
 if [ ! -e ubuntu-rootfs.tar.gz ]; then
     wget -O ubuntu-rootfs.tar.gz http://cdimage.ubuntu.com/ubuntu-touch/daily-preinstalled/current/saucy-preinstalled-touch-armhf.tar.gz
 fi
