6f247f5e4e92bc777e9e10be336b5917bf26a76d
Dmitrijs Ledkovs <dmitrijs.ledkovs@canonical.com>
2013-10-28 17:14:04 +0000

    Remove hacks from the sd-card build-script, hacks upstreamed into their proper locations.

--- tools/build-emulator-sdcard.sh
+++ tools/build-emulator-sdcard.sh
@@ -3,30 +3,19 @@ set -e
 set -x
 [ -z $OUT ] && exit 1
 if [ ! -e $OUT/ubuntu-rootfs.tar.gz ]; then
-    wget -O $OUT/ubuntu-rootfs.tar.gz http://cdimage.ubuntu.com/ubuntu-touch/daily-preinstalled/current/saucy-preinstalled-touch-armhf.tar.gz
+    wget -O $OUT/ubuntu-rootfs.tar.gz http://cdimage.ubuntu.com/ubuntu-touch/daily-preinstalled/pending/trusty-preinstalled-touch-armhf.tar.gz
 fi
-mksdcard -l UBUNTU 3G $OUT/ubuntu-system.img
+
+dd if=/dev/zero of=$OUT/ubuntu-system.img bs=1 count=1 seek=3G
 mkfs.ext4 -F -L UBUNTU $OUT/ubuntu-system.img
 
-mksdcard -l USERDATA 4G $OUT/sdcard.img
+dd if=/dev/zero of=$OUT/sdcard.img bs=1 count=1 seek=4G
 mkfs.ext4 -F -L USERDATA $OUT/sdcard.img
 
 sudo mount $OUT/ubuntu-system.img /mnt
 sudo tar --numeric-owner -xzf $OUT/ubuntu-rootfs.tar.gz -C /mnt/
 
-sudo sh -c "cat > /mnt/etc/init/ttyS2.conf" <<EOF
-start on startup
-stop on runlevel [!12345]
-
-respawn
-exec /sbin/getty -L 115200 ttyS2 vt102
-EOF
-
-sudo sh -c "cat > /mnt/usr/lib/lxc-android-config/70-generic.rules" <<EOF
-ACTION=="add", KERNEL=="qemu_trace", OWNER="system", GROUP="system", MODE="0666"
-ACTION=="add", KERNEL=="qemu_pipe", OWNER="system", GROUP="system", MODE="0666"
-ACTION=="add", KERNEL=="ttyS*", OWNER="system", GROUP="system", MODE="0666"
-EOF
+sudo sh -c "echo manual > /mnt/etc/init/bluetooth.override"
 
 sudo cp $OUT/system.img /mnt/var/lib/lxc/android/system.img
 
